b89cd12d4456d89ec6376840924173ec66ec833d
cazerta <marcelo.cazerta@gmail.com>
2018-04-17 14:23:05 -0300

    Balance of Power

--- StandardMinUi.lua
+++ StandardMinUi.lua
@@ -1,27 +1,70 @@
+local uiConfig = {
+	minAlpha = 0.7,
+	maxAlpha = 1,
+	mainMenuScale = 0.7,
+	playerTargetScale = 1.1,
+	playerTargetFrame_X = 250,
+	playerTargetFrame_Y = 220
+}
+
 --MENUBAR 
-MainMenuBarLeftEndCap:Hide() --Hides left gargoyle
-MainMenuBarRightEndCap:Hide() --Hides right
-MainMenuBar:SetScale(0.7) --scale down the bar
-MainMenuBar:SetAlpha(0.7) --70% visible
+MainMenuBarLeftEndCap:Hide();
+MainMenuBarRightEndCap:Hide();
+MainMenuBar:SetScale(uiConfig.mainMenuScale);
+MainMenuBar:SetAlpha(uiConfig.minAlpha) ;
 
 --PLAYER & TARGET frames
-PlayerFrame:SetScale(1.1); --increase size 10%
+PlayerFrame:SetScale(uiConfig.playerTargetScale);
 PlayerFrame:ClearAllPoints();
-PlayerFrame:SetPoint("CENTER",UIParent,-250,-220); --Sets position x, y
+PlayerFrame:SetPoint("CENTER",UIParent,-uiConfig.playerTargetFrame_X,-uiConfig.playerTargetFrame_Y);
 PlayerFrame:SetUserPlaced(true);
 
-TargetFrame:SetScale(1.1); --increase size 10%
+TargetFrame:SetScale(uiConfig.playerTargetScale);
 TargetFrame:ClearAllPoints();
-TargetFrame:SetPoint("CENTER",UIParent,250,-220); --Sets position x, y
+TargetFrame:SetPoint("CENTER",UIParent,uiConfig.playerTargetFrame_X,-uiConfig.playerTargetFrame_Y); 
 TargetFrame:SetUserPlaced(true);
 
---Esconde objetivos se estiver em raid
+--Frame to deal with instances and check IsInRaid
 local f = CreateFrame("FRAME");
 f:RegisterEvent("PLAYER_ENTERING_WORLD"); --Event that fires on loadingscreens
+f:RegisterEvent("RAID_ROSTER_UPDATE");
+f:RegisterEvent("PARTY_MEMBERS_CHANGED");
 f:SetScript("OnEvent", function(self, event, ...)
+	print('fired')
+	print(IsInRaid())
+	
+	--print('UnitInRaid: ' .. UnitInRaid('player'))
 	if IsInRaid() then --if player is in raid hides objective tracker else shows it
+		print("TRIGGER InRaind() - Escondendo objetivos")
 		ObjectiveTrackerFrame:Hide();
 	else
 		ObjectiveTrackerFrame:Show();
 	end
 end)
+
+local balancePowerQuests = {
+    ["[1] The Power Within - "] = 43496,
+    ["[2] Eye of Azshara: The Heart of Zin-Azshari - "] = 40668,
+    ["[3] Darkheart Thicket: Fallen Power - "] = 43517,
+    ["[4] Lucid Strength - "] = 43519,
+    ["[5] The Emerald Nightmare: In Nightmares - "] = 43520,
+    ["[6] Essential Consumption - "] = 43522,
+    ["[6.5] Saving the Guard - "] = 43527,
+    ["[7] Repaid Debt - "] = 43523,
+    ["[8] Lost Knowledge - "] = 40673
+}
+
+local function checkBalancePower() 
+    for index,value in pairs(balancePowerQuests) do
+        if (IsQuestFlaggedCompleted(value)) then
+            print("|cff00ff00" .. index)
+        else
+            print("|cffff0000" .. index)
+        end
+    end
+end
+
+SLASH_BALANCEPOWER1 = '/smbop'
+function SlashCmdList.BALANCEPOWER(msg, editbox)
+    checkBalancePower()
+end
